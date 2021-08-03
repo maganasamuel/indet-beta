b5458239f70c669808e61ec61073c45c571e0f9f
Samuel Magana <maganasamuel001@gmail.com>
2021-08-04 11:08:50 +1200

    Get upcoming birthdays on next month 15 days

--- libs/api/classes/magazine.class.php
+++ libs/api/classes/magazine.class.php
@@ -4221,17 +4221,9 @@ class Magazine extends Database
     {
         $output = [];
 
-        $date = Carbon::createFromFormat('Ymd', $this->date);
-
-        $day = (int) $date->format('d');
-
-        if ($day >= 1 && $day <= 15) {
-            $from = $date->format('m') . str_pad($day, 2, '0', STR_PAD_LEFT);
-            $to = $date->format('m') . 15;
-        } else {
-            $from = $date->format('m') . 16;
-            $to = $date->endOfMonth()->format('md');
-        }
+        $from = Carbon::createFromFormat('Ymd', $this->bimonthRange->from)->addMonths(1);
+        
+        $to = Carbon::createFromFormat('Ymd', $this->bimonthRange->to)->addMonths(1)->endOfMonth();
 
         //Get Admins
         $query = "SELECT p.full_name as name, p.image as image, p.role, p.birthday FROM personal_data p WHERE RIGHT(p.birthday,4) >= '$from' AND RIGHT(p.birthday,4) <= '$to' AND p.termination_date = '' ORDER BY name";

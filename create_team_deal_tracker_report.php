b5fb6f4fa072dde89c31bb3e09097dda1084debc
Samuel Magana <samuel.magana@ziaplex.biz>
2021-10-15 21:20:10 +1300

    Only put eliteinsure team from team options

--- create_team_deal_tracker_report.php
+++ create_team_deal_tracker_report.php
@@ -3,29 +3,29 @@
 session_start();
 
 //Restrict access to admin only
-include "partials/admin_only.php";
+include 'partials/admin_only.php';
 
-$_SESSION["x"] = 1;
+$_SESSION['x'] = 1;
 unset($_SESSION['adviser_id']);
-if (!isset($_SESSION["myusername"])) {
-	session_destroy();
-	header("Refresh:0; url=index.php");
-} else {
-	?>
-	<html>
 
+if (! isset($_SESSION['myusername'])) {
+    session_destroy();
+    header('Refresh:0; url=index.php');
+} else {
+    ?>
+	<html>
 	<head>
 		<!--nav bar-->
-		<?php include "partials/nav_bar.html"; ?>
+		<?php include 'partials/nav_bar.html'; ?>
 
 		<!--nav bar end-->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
-
 		<link rel="stylesheet" href="styles.css">
 		<link id="favicon" rel="icon" href="Logo_ImageOnly.png" type="image/png" sizes="16x16">
 		<title>INDET</title>
+
 		<style>
 			div#myModal {
 				padding-top: 0px !important;
@@ -41,7 +41,6 @@ if (!isset($_SESSION["myusername"])) {
 			}
 
 			.modal-content {
-
 				width: 100% !important;
 				height: auto;
 				min-height: 100%;
@@ -51,7 +50,6 @@ if (!isset($_SESSION["myusername"])) {
 
 		<script>
 			$(document).ready(function() {
-
 				var advisers = [];
 
 				$("#datefrom").datepicker({
@@ -77,7 +75,7 @@ if (!isset($_SESSION["myusername"])) {
 						return new Date(y, m + 1, 0).getDate();
 					}
 
-					console.log(ifday);
+					// console.log(ifday);
 
 					if (ifday == 16) {
 						//First Half
@@ -86,7 +84,7 @@ if (!isset($_SESSION["myusername"])) {
 						var next_year = year;
 
 						if (next_month > 12) {
-							console.log("Next Month:" + next_month);
+							// console.log("Next Month:" + next_month);
 							next_month = 1;
 							next_year = year + 1;
 						}
@@ -97,14 +95,10 @@ if (!isset($_SESSION["myusername"])) {
 					} else {
 						//Second Half
 						var ld = '15' + '/' + n(m) + '/' + year;
-
-
 						var pd = '21' + '/' + n(m) + '/' + year;
 						$('#dateuntil').val(ld);
 						$('#pay_date').val(pd);
 					}
-
-
 				});
 
 				function n(n) {
@@ -120,7 +114,7 @@ if (!isset($_SESSION["myusername"])) {
 						console.log(data);
 						advisers = data.adviser_ids;
 					});
-				});
+				}).change();
 
 				$('#create').on('click', function(e) {
 					e.preventDefault();
@@ -135,7 +129,8 @@ if (!isset($_SESSION["myusername"])) {
 						return false;
 					}
 
-					console.log(date_from + ":" + until);
+					// console.log(date_from + ":" + until);
+
 					$.ajax({
 						dataType: 'json',
 						type: 'POST',
@@ -191,42 +186,24 @@ if (!isset($_SESSION["myusername"])) {
 							console.log("Data:" + x);
 						}
 					});
-
 				});
-
 			});
 		</script>
-
 	</head>
-
 	<body>
-
-		<!--header-->
 		<div align="center">
-
-
-
-			<!--header end-->
-
-			<!--nav bar-->
-
-			<!--nav bar end-->
-
-			<!--label-->
 			<div class="jumbotron">
 				<h2 class="slide">Create Team Policy Tracker Report</h2>
 			</div>
-			<!--label end-->
+    <?php
+    require 'database.php';
+    $con = mysqli_connect($host, $username, $password, $db) or die('could not connect to sql');
 
-			<?php require "database.php";
-				$con = mysqli_connect($host, $username, $password, $db) or die("could not connect to sql");
-				if (!$con) {
-					echo "<div>";
-					echo "Failed to connect to MySQL: " . mysqli_connect_error();
-					echo "</div>";
-				}
-
-				?>
+    if (! $con) {
+        echo '<div>';
+        echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+        echo '</div>';
+    } ?>
 			<form method="POST" action="deal_report_preview.php" autocomplete="off" class="margined">
 				<table align="center" bgcolor="ededed" cellpadding="5px" onload="form1.reset();">
 					<div class='row'>
@@ -237,16 +214,20 @@ if (!isset($_SESSION["myusername"])) {
 								<div class="input-group">
 									<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 									<select name="team" class="form-control" id="team" required />
-									<option value="" disabled selected>Select Team</option>
-									<?php
-										$query = "SELECT id,name FROM teams ORDER BY name asc";
-										$displayquery = mysqli_query($con, $query) or die('Could not look up user information; ' . mysqli_error($con));
-										while ($rows = mysqli_fetch_array($displayquery)) {
-											$id = $rows["id"];
-											$name = $rows["name"];
-											echo "<option value='$id'>" . $name . "</option>";
-										}
-										?>
+	    								<!-- <option value="" disabled selected>Select Team</option> -->
+    <?php
+    $query = 'SELECT id,name FROM teams ORDER BY name asc';
+    $displayquery = mysqli_query($con, $query) or die('Could not look up user information; ' . mysqli_error($con));
+    while ($rows = mysqli_fetch_array($displayquery)) {
+        $id = $rows['id'];
+        $name = $rows['name'];
+
+        if (8 != $id) {
+            continue;
+        }
+
+        echo "<option value='$id' selected>" . $name . '</option>';
+    } ?>
 									<!--<td><input class="addadviser" type="text" id="datepicker" name="mydate" required></td>-->
 									</select>
 									<input type='hidden' name='adv_name' id='adv_name' value=''>
@@ -261,7 +242,6 @@ if (!isset($_SESSION["myusername"])) {
 										<i class="fa fa-calendar" aria-hidden="divue"></i></span>
 									<input class="form-control datepicker" autocomplete="off" type="text" id="datefrom" name="date_from" /></div>
 							</label>
-
 						</div>
 						<div class='col-sm-2'>
 							<label>Until
@@ -279,25 +259,15 @@ if (!isset($_SESSION["myusername"])) {
 									<input class="form-control" autocomplete="off" type="text" id="pay_date" name="pay_date" readonly />
 								</div>
 							</label>
-
 						</div>
-
 					</div>
-
-
-
 					<div class="row">
 						<div class="col-sm-2 center">
-
 							<button type="button" class="btn btn-danger center form-control" id='create' style="margin-top:30px;"> <i id="create_spinner" class="fas fa-spinner fa-spin" style="display:none;"></i> Create Report</button>
-
 						</div>
 					</div>
 			</form>
-
-
 			<div class="container">
-
 				<!-- Modal -->
 				<div class="modal fade" id="myModal" role="dialog" style="z-index:10000;width: 100%;">
 					<div class="modal-dialog modal-lg">
@@ -306,9 +276,7 @@ if (!isset($_SESSION["myusername"])) {
 								<button type="button" class="close" data-dismiss="modal">&times;</button>
 								<h2 class="modal-title" style="float: left;">Policy Report Preview</h2>
 							</div>
-							<div class="modal-body">
-
-							</div>
+							<div class="modal-body"></div>
 							<div class="modal-footer">
 								<button type="button" class="btn btn-info" id='save_pdf'><i id="save_pdf_spinner" class="fas fa-spinner fa-spin" style="display:none;"></i> Save</button>
 								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -317,16 +285,8 @@ if (!isset($_SESSION["myusername"])) {
 					</div>
 				</div>
 			</div>
-
-
-
 	</body>
-
-
 	</html>
-
 <?php
-
 }
-
 ?>
